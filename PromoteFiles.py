f487fc01284d90e60c2ace31df504f63693db37b
Ryan Ottney <rottney123@gmail.com>
2019-10-10 20:27:57 -0700

    still broken
    still very broken...

--- PromoteFiles.py
+++ PromoteFiles.py
@@ -1,6 +1,7 @@
 import sys
 import time
-import ntpath
+# consider ntpath
+import os
 from watchdog.utils.dirsnapshot import DirectorySnapshot
 from watchdog.utils.dirsnapshot import DirectorySnapshotDiff
 
@@ -8,15 +9,8 @@ if __name__ == "__main__":
 
 	path = '.'
 
-	#	KNOWN ISSUE:  There's one caveat: Linux filenames may contain backslashes.
-	#	So on linux, r'a/b\c' always refers to the file b\c in the a folder,
-	#	while on Windows, it always refers to the c file in the b subfolder of the a folder.
-	#	So when both forward and backward slashes are used in a path,
-	#	you need to know the associated platform to be able to interpret it correctly.
-
-	#	https://stackoverflow.com/questions/8384737/extract-file-name-from-path-no-matter-what-the-os-path-format
-	if not ntpath.exists('./snapshot.txt'):
-		with open('./snapshot.txt', 'a'): pass			# do I have to close this?
+	if not os.path.exists('./snapshot.dat'):
+		with open('./snapshot.dat', 'a'): pass			# do I have to close this?
 
 	snapshot = DirectorySnapshot(path, recursive=True)
 
@@ -34,7 +28,7 @@ if __name__ == "__main__":
 				else:
 					# Created files
 					for file in diff.files_created:
-						if (file.endswith(".txt")):		# add case for when file name is ./snapshot.txt
+						if (file.endswith(".txt")):
 							print(file + " has been created.")
 						else:
 							print("A new file has been created in this directory, but this change is not promotable because only .txt files are supported by this application.")
@@ -43,13 +37,15 @@ if __name__ == "__main__":
 					for file in diff.files_modified:	# add case for when file name is ./snapshot.txt
 						if (file.endswith(".txt")):
 							print(file + " has been modified.")
+						elif (os.path.split("./snapshot.dat")):
+							print("handle yo input, boi...")
 						else:
 							print("A new file has been modified in this directory, but this change is not promotable because only .txt files are supported by this application.")
 
 					snapshot = current
 
 					# Save snapshot to file
-					file = open('./snapshot.txt', 'w')
+					file = open('./snapshot.dat', 'w')
 					file.write(str(snapshot))
 					file.close()
 
